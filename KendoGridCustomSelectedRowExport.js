e928c1cd06aab6a4931ef49e203231a3b3a2ca00
Bhanu Pratap <34159334+erbpsy@users.noreply.github.com>
2021-01-10 22:43:30 +0530

    Export selected rows to excel
    Export custom selected rows to excel or all data source with custom columns we can export rows or columns which are required to export it is fully customizable.
    For any extra help send your query.

---
+++ KendoGridCustomSelectedRowExport.js
@@ -0,0 +1,76 @@
+ function excelExport() {
+        var exportAll = $('.selectrow').is(":checked");
+        var grid = $("#grid");
+        var rows = [{
+            cells: [
+                { value: "column1" },
+                { value: "column2" },
+                { value: "column3" },
+                { value: "column4" },
+                { value: "column5" },
+                { value: "column6" },
+                { value: "column7" }
+            ]
+        }];
+        if (exportAll) {
+            var dataDource = grid.getKendoGrid();
+            var trs = $("#gridResultTransaction").find('tr');
+            for (var i = 0; i < trs.length; i++) {
+                if ($(trs[i]).find(":checkbox").is(":checked")) {
+                    var dataItem = dataDource.dataItem(trs[i]);
+                    rows.push({
+                        cells: [
+                            { value: dataItem.column1 },
+                            { value: dataItem.column2 },
+                            { value: dataItem.column3 },
+                            { value: dataItem.column4 },
+                            { value: dataItem.column5 },
+                            { value: dataItem.column6 },
+                            { value: dataItem.column7 }
+                        ]
+                    })
+                }
+            }
+        }
+        else {
+            var dataSource = grid.data("kendoGrid").dataSource;
+            var trs = grid.find('tr');
+            for (var i = 1; i < dataSource._data.length; i++) {
+                    var dataItem = dataSource._data[i];
+                    rows.push({
+                        cells: [
+                            { value: dataItem.column1 },
+                            { value: dataItem.column2 },
+                            { value: dataItem.column3 },
+                            { value: dataItem.column4 },
+                            { value: dataItem.column5 },
+                            { value: dataItem.column6 },
+                            { value: dataItem.column7 }
+                        ]
+                    })
+            }
+        }
+
+
+
+        var workbook = new kendo.ooxml.Workbook({
+            sheets: [
+                {
+                    columns: [
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true },
+                        { autoWidth: true }
+                    ],
+                    title: "Exported Data Result",
+                    rows: rows
+                }
+            ]
+        });
+        kendo.saveAs({ dataURI: workbook.toDataURL(), fileName: "ExportedData" });
+    }
